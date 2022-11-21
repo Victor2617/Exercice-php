53cd27319fb136d89a1855c43b162c2875cc3669
Victor2617 <Victor2617>
2022-11-21 12:13:27 +0100

    EX1-php

--- index.php
+++ index.php
@@ -9,7 +9,32 @@
     <script src='main.js'></script>
 </head>
 <body>
-    Coucou Victor
-    <h1>mon titre1 </h1>
+<?php 
+            $NombreAleatoire = rand ( 0 , 100 );
+            ?>
+            <p> Si mon nombre aléatoire est paire j'affiche du rouge, sinon j'affiche du bleu</p>
+            <?php
+            if ($NombreAleatoire%2 == 1)
+            {
+            ?>
+                <div id = 'impair'>
+                    <?php
+                    echo $NombreAleatoire." est impair";
+                    ?>
+                </div>
+            <?php
+            }
+            else
+            {
+            ?>
+                <div id = 'pair'>
+                    <?php
+                    echo $NombreAleatoire." est pair";
+                    ?>
+                </div>
+            <?php
+            }
+
+        ?>
 </body>
 </html>
